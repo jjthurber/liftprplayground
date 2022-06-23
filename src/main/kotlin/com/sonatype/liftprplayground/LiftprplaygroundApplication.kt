a777c4b25e8440149a0cf025c9b48eee8cc9d0c4
Chris Wininger <cwininger@sonatype.com>
2022-06-23 16:17:07 -0400

    first commit

---
+++ src/main/kotlin/com/sonatype/liftprplayground/LiftprplaygroundApplication.kt
@@ -0,0 +1,13 @@
+package com.sonatype.liftprplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiftprplaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<LiftprplaygroundApplication>(*args)
+
+	throw RuntimeException("Not yet implemented, not to mention lift does not like RuntimeException")
+}
---
+++ src/test/kotlin/com/sonatype/liftprplayground/LiftprplaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sonatype.liftprplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiftprplaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
