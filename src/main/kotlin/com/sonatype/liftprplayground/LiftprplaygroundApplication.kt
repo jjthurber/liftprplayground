4f468179e5a033da90f501d32d11c5a34205bfc4
Chris Wininger <cwininger@sonatype.com>
2022-06-24 11:30:58 -0400

    should fix two existing problems

--- src/main/kotlin/com/sonatype/liftprplayground/LiftprplaygroundApplication.kt
+++ src/main/kotlin/com/sonatype/liftprplayground/LiftprplaygroundApplication.kt
@@ -9,7 +9,5 @@ class LiftprplaygroundApplication
 fun main(args: Array<String>) {
 	runApplication<LiftprplaygroundApplication>(*args)
 
-	throw RuntimeException("Not yet implemented, not to mention lift does not like RuntimeException")
-
 	throw RuntimeException("Introducing another bug for the squirrel")
 }
