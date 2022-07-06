1ebbc2684b82a0d930c6748f243d0e75e577ed56
Chris Wininger <cwininger@sonatype.com>
2022-07-06 17:08:41 -0400

    much fun

---
+++ custom-tool.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+JAVA=/usr/lib/jvm/java-11-openjdk-amd64/bin/java
+
+dir=$1
+commit=$2
+cmd=$3
+
+function tellName() {
+  return "Environment Export"
+  return 0
+}
+
+function tellApplicable() {
+  echo "true"
+  return 0
+}
+
+function tellVersion() {
+  echo "1"
+  return 0
+}
+
+function run() {
+  echo "{"
+  echo "\"type\" : \"baz\","
+  echo "\"message\" : \"value: $LIFT_ANALYSIS_API_URL\","
+  echo "\"file\" : \"foo.bar\","
+  echo "\"line\" : 1"
+  echo "}"
+}
+
+if [[ "$cmd" = "name" ]] ; then
+  tellName
+fi
+
+if [[ "$cmd" = "run" ]] ; then
+        run
+fi
+if [[ "$cmd" = "applicable" ]] ; then
+        tellApplicable
+fi
+if [[ "$cmd" = "version" ]] ; then
+        tellVersion
+fi
+
