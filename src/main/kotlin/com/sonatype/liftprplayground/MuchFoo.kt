011822ccbee066e2b96c7045e701ebf461aa0231
Chris Wininger <cwininger@sonatype.com>
2022-08-17 16:05:31 -0400

    throw bad stuff wow, much evil

---
+++ src/main/kotlin/com/sonatype/liftprplayground/MuchFoo.kt
@@ -0,0 +1,23 @@
+package com.sonatype.liftprplayground
+
+class MuchFoo {
+  fun myFunction() {
+    throw RuntimeException("1")
+  }
+
+  fun myFunction1() {
+    throw RuntimeException("2")
+  }
+
+  fun myFunction2() {
+    throw RuntimeException("3")
+  }
+
+  fun myFunction3() {
+    throw RuntimeException("4")
+  }
+
+  fun myFunction4() {
+    throw RuntimeException("5")
+  }
+}
