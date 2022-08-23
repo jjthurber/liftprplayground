fd50720bc2701523bd71901301b1ee6244437c50
Chris Wininger <cwininger@sonatype.com>
2022-08-23 16:38:14 -0400

    keep trying to make lift mad

---
+++ temp.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+  echo "{"
+  echo "\"type\" : \"baz\","
+  echo "\"message\" : \"value: $LIFT_ANALYSIS_API_URL\","
+  echo "\"file\" : \"foo.bar\","
+  echo "\"line\" : 1"
+  echo "}"
