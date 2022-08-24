b01b69d37a8a10fc933b3c9d324d84df8cdaffd0
Chris Wininger <cwininger@sonatype.com>
2022-08-24 14:52:06 -0400

    added several generic exceptions to a single file

---
+++ src/main/kotlin/com/sonatype/liftprplayground/MuchFoo.kt
@@ -0,0 +1,19 @@
+package com.sonatype.liftprplayground
+
+class MuchFoo {
+  fun myFunction() {
+    throw RuntimeException("1")
+  }
+
+  fun myFunction1() {
+    throw RuntimeException("2")
+  }
+
+  fun myFunction2() {
+    throw RuntimeException("3")
+  }
+
+  fun myFunction3() {
+    throw RuntimeException("4")
+  }
+}
