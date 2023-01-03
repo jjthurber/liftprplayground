f0a2fd55a341955353b3e8520438b74a8a6f338e
John Thurber <32336792+jjthurber@users.noreply.github.com>
2023-01-03 10:55:04 -0500

    Update LiftprplaygroundApplication.kt

--- src/main/kotlin/com/sonatype/liftprplayground/LiftprplaygroundApplication.kt
+++ src/main/kotlin/com/sonatype/liftprplayground/LiftprplaygroundApplication.kt
@@ -3,6 +3,8 @@ package com.sonatype.liftprplayground
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+var name = "John"
+
 @SpringBootApplication
 class LiftprplaygroundApplication
 
